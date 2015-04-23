c67e6a440f07a00ede48baf4d903cbd42f88a1c5
Matthias <matthias.moulin@gmail.com>
2015-04-23 11:40:39 +0200

    Update

--- src/quad.py
+++ src/quad.py
@@ -70,3 +70,44 @@ def clenshawcurtis(f, n):                                         # (n+1)-pt qua
     w = np.zeros(a.shape)                                         # weight vector
     w[::2] = 2.0 / (1.0 - np.arange(0, n+1, 2)**2)                # 
     return np.sum(w * a)                                         # integral value
+    
+###############################################################################
+# ROMBERG METHOD
+###############################################################################
+def romberg(f, n, lo=-1, hi=1):
+    return rombergtable(f, n, lo=lo, hi=hi)[n,n]
+
+def rombergtable(f, n, lo=-1, hi=1):
+    T = np.zeros((n + 1, n + 1))
+    h = hi - lo
+    T[0,0] = 0.5 * h * (f(lo) + f(hi))
+    p2 = 1
+    for i in range(1, n + 1):
+        h = 0.5 * h
+        p2 = 2 * p2
+        
+        #Odd contributions
+        tsum = 0.0
+        for k in range(1, p2, 2):
+            tsum = tsum + f(lo + k * h)
+        
+        #Compound trapezoid rule via Richardson extrapolation
+        T[i,0] = 0.5 * T[i-1,0] + h * tsum
+        
+        p4 = 1
+        for j in range(1, i + 1):
+            p4 = 4 * p4
+            T[i,j] = T[i,j-1] + (T[i,j-1] - T[i-1,j-1]) / (p4 - 1) #Instead of p19
+    return T
+    
+def vis_romberg(f, n, lo=-1, hi=1):
+    T = rombergtable(f, n, lo=lo, hi=hi)
+    h = hi - lo
+    p2 = 1
+    print('Steps\tStep size\tResults')
+    for i in range(0, n + 1):
+        print(str(p2) + '\t' + str(h) + '\t\t' + '\t'.join(str(cell) for cell in T[i,0:i+1]) + '\n')
+        h = 0.5 * h
+        p2 = 2 * p2
+        
+  
