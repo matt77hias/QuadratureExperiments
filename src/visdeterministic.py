b12c6f1148dc4b10fce51119d97e561bc947f5b8
Matthias <matthias.moulin@gmail.com>
2015-04-23 12:14:40 +0200

    Update

--- src/visdeterministic.py
+++ src/visdeterministic.py
@@ -10,16 +10,19 @@ import quad
 def vis_relative_error(f, I, ns=range(1, 31)):
     Egs = np.zeros(len(ns))
     Ecs = np.zeros(len(ns))
-    Ers = np.zeros(len(ns))
     for j in range(len(ns)):
         Egs[j] = np.divide(abs(I - quad.gausslegendre(f, ns[j])), I)
         Ecs[j] = np.divide(abs(I - quad.clenshawcurtis(f, ns[j])), I)
+    
+    nr = range(1, 11)
+    Ers = np.zeros(len(nr))
+    for j in range(len(nr)):
         Ers[j] = np.divide(abs(I - quad.romberg(f, ns[j])), I)
     
     plt.figure()
     plt.semilogy(ns, Egs, label='gauss-legendre', color='g', marker='o', ls='-')
     plt.semilogy(ns, Ecs, label='clenshaw-curtis', color='b', marker='o', ls='-')
-    plt.semilogy(ns, Ers, label='romberg', color='r', marker='o', ls='-')
+    plt.semilogy(nr, Ers, label='romberg', color='r', marker='o', ls='-')
     plt.legend(loc=1)
     plt.title('Relative error ' + f.func_name + '(x)')
     plt.xlabel('n')
@@ -29,16 +32,19 @@ def vis_relative_error(f, I, ns=range(1, 31)):
 def vis_absolute_error(f, I, ns=range(1, 31)):
     Egs = np.zeros(len(ns))
     Ecs = np.zeros(len(ns))
-    Ers = np.zeros(len(ns))
     for j in range(len(ns)):
         Egs[j] = abs(I - quad.gausslegendre(f, ns[j]))
         Ecs[j] = abs(I - quad.clenshawcurtis(f, ns[j]))
+    
+    nr = range(1, 11)
+    Ers = np.zeros(len(nr))
+    for j in range(len(nr)):
         Ers[j] = abs(I - quad.romberg(f, ns[j]))
     
     plt.figure()
     plt.semilogy(ns, Egs, label='gauss-legendre', color='g', marker='o', ls='-')
     plt.semilogy(ns, Ecs, label='clenshaw-curtis', color='b', marker='o', ls='-')
-    plt.semilogy(ns, Ers, label='romberg', color='r', marker='o', ls='-')
+    plt.semilogy(nr, Ers, label='romberg', color='r', marker='o', ls='-')
     plt.legend(loc=1)
     plt.title('Absolute error: ' + f.func_name + '(x)')
     plt.xlabel('n')
@@ -48,16 +54,19 @@ def vis_absolute_error(f, I, ns=range(1, 31)):
 def vis_sds(f, I, ns=range(1, 31)):
     Ngs = np.zeros(len(ns))
     Ncs = np.zeros(len(ns))
-    Nrs = np.zeros(len(ns))
     for j in range(len(ns)):
         Ngs[j] = np.log10(np.divide(abs(I - quad.gausslegendre(f, ns[j])), I))
         Ncs[j] = np.log10(np.divide(abs(I - quad.clenshawcurtis(f, ns[j])), I))
+    
+    nr = range(1, 11)
+    Nrs = np.zeros(len(nr))
+    for j in range(len(nr)):
         Ncs[j] = np.log10(np.divide(abs(I - quad.romberg(f, ns[j])), I))
     
     plt.figure()
     plt.plot(ns, Ngs, label='gauss-legendre', color='g', marker='o', ls='-')
     plt.plot(ns, Ncs, label='clenshaw-curtis', color='b', marker='o', ls='-')
-    plt.plot(ns, Nrs, label='romberg', color='r', marker='o', ls='-')
+    plt.plot(nr, Nrs, label='romberg', color='r', marker='o', ls='-')
     plt.legend(loc=1)
     plt.title('Number of significant digits ' + f.func_name + '(x)')
     plt.xlabel('n')
@@ -66,16 +75,24 @@ def vis_sds(f, I, ns=range(1, 31)):
     
 def nb_of_functionevaluations(f, I, s=-7, ns=range(1, 101)):
     Ng = Nc = Nr = -1
+    nr = range(1, 11)
+    
     for j in range(len(ns)):
         sg = np.log10(np.divide(abs(I - quad.gausslegendre(f, ns[j])), I))
-        if (Ng == -1 and sg <= s): 
-            Ng = ns[j] + 1 
-        sc = np.log10(np.divide(abs(I - quad.clenshawcurtis(f, ns[j])), I))
-        if (Nc == -1 and sc <= s): 
-            Nc = ns[j] + 1
-        sr = np.log10(np.divide(abs(I - quad.romberg(f, ns[j])), I))
-        if (Nr == -1 and sr <= s): 
-            Nr = 2**(ns[j] + 1) + 1
-        if (Ng != -1 and Nc != -1 and Nr != -1): 
+        if (sg <= s): 
+            Ng = ns[j] + 1
             break
+    
+    for j in range(len(ns)):
+        sc = np.log10(np.divide(abs(I - quad.clenshawcurtis(f, ns[j])), I))
+        if (sc <= s): 
+            Nc = ns[j] + 1
+            break
+    
+    for j in range(len(nr)):
+        sr = np.log10(np.divide(abs(I - quad.romberg(f, ns[j])), I))
+        if (sr <= s): 
+            Nr = 2**(ns[j] + 1) + 1
+            break
+    
     return (Ng, Nc, Nr)
